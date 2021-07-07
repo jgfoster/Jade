745576ea3098daa2fb2860fe5993104ef9f718ba
James Foster <git@jgfoster.net>
2021-07-07 16:58:05 -0700

    Experiment with WebSockets.

--- lib/jade_server.dart
+++ lib/jade_server.dart
@@ -1,16 +1,23 @@
 import 'dart:convert';
-import 'dart:io';
 import 'dart:typed_data';
+
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:web_socket_channel/status.dart' as status;
 
 class JadeServer {
   var _isInitialized = false;
-  late final _socket;
+  late final _channel;
 
   JadeServer(var host, var port) {
     initialize(host, port);
   }
 
-  // Future<Map<String, dynamic>> _read() async {}
+  Future<Map<String, dynamic>> _read() async {
+    var stream = _channel.stream;
+    var version = stream.take(1).first;
+    print(version);
+    return {};
+  }
 
   Future<void> _write(var map) async {
     await waitForInitialization();
@@ -19,17 +26,19 @@ class JadeServer {
     var byteData = ByteData.view(byteList.buffer);
     byteData.setUint8(0, 1);
     byteData.setUint32(1, data.length);
-    await _socket.add(byteList);
-    await _socket.write(data);
+    await _channel.sink.add(byteList);
+    await _channel.sink.add(data);
   }
 
   Future<void> initialize(var host, var port) async {
-    _socket = await SecureSocket.connect(host, port);
+    var uri = Uri(scheme: 'wss', host: host, port: port);
+    _channel = WebSocketChannel.connect(uri);
     _isInitialized = true;
   }
 
   getGciVersion() async {
     await _write({'request': 'getGciVersion'});
+    await _read();
   }
 
   Future<void> waitForInitialization() async {
